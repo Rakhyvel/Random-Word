ac7051a4bb1f7d3f82439069339e12ea14906e65
josephs1203 <josephs1203@OSCR-SUMC-28.catnet.arizona.edu>
2019-04-17 16:10:44 -0700

    Added adaptive learning

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,17 +13,25 @@ public class Main {
 
 	public static void main(String[] args) {
 		Main m = new Main();
-		System.out.println(m.randName(500));
+		System.out.println(m.randName(500,10));
 	}
 
-	public String randName(int length) {
-		String name = "";
-		System.out.println(getHashes());
-
-		return name;
+	public String randName(int length, int portionSize) {
+		HashMap<String, Double> morphemes = getHashes(portionSize);
+		List<String> keysAsArray = new ArrayList<String>(morphemes.keySet());
+		String portion = keysAsArray.get(rand.nextInt(keysAsArray.size())).substring(0,portionSize-1);
+		String name = portion;
+		for(int i = 0; i < length; i++) {
+			String randPortion = spitOutPossibilities(morphemes, portion, portionSize);
+			if(randPortion != null && randPortion != "") {
+				name += randPortion.charAt(portionSize-1);
+				portion = randPortion.substring(1,portionSize);
+			}
+		}
+		return name.substring(0,1).toUpperCase() + name.substring(1,name.length());
 	}
 
-	String[][] getLetterList() {
+	HashMap<String, Double> getHashes(int portionSize) {
 		// Reading the file
 		List<String> records = new ArrayList<String>();
 		String filename = "src/main/input.txt";
@@ -41,67 +49,54 @@ public class Main {
 		}
 
 		// Converting the strings into seperated values
-		String[][] probabilities = new String[records.size()][2];
+		HashMap<String, Double> probabilities = new HashMap<String, Double>();
 		for (int i = 0; i < records.size(); i++) {
-			for (int i2 = 0; i2 < records.get(i).length() - 2; i2++) {
-				System.out.println(records.get(i).substring(i2, i2 + 3));
-			}
-		}
-		return probabilities;
-	}
-
-	HashMap<String, Integer> getHashes() {
-		// Reading the file
-		List<String> records = new ArrayList<String>();
-		String filename = "src/main/input.txt";
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(filename));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				records.add(line);
-			}
-			reader.close();
-		} catch (Exception e) {
-			System.err.format("Exception occurred trying to read '%s'.", filename);
-			e.printStackTrace();
-			return null;
-		}
-
-		// Converting the strings into seperated values
-		HashMap<String, Integer> probabilities = new HashMap<String, Integer>();
-		for (int i = 0; i < records.size(); i++) {
-			for (int i2 = 0; i2 < records.get(i).length() - 2; i2++) {
-				String sub = records.get(i).substring(i2, i2 + 3);
+			for (int i2 = 0; i2 < records.get(i).length() - portionSize + 1; i2++) {
+				String sub = records.get(i).substring(i2, i2 + portionSize);
 				if(probabilities.containsKey(sub)){
 					probabilities.replace(sub, probabilities.get(sub)+1);
 				} else {
-					probabilities.put(sub, 1);
+					probabilities.put(sub, 1.0);
 				}
 			}
 		}
 		return probabilities;
 	}
+	
+	String spitOutPossibilities(HashMap<String, Double> probabilities, String target, int portionSize) {
+		ArrayList<String> lettersList = new ArrayList<String>();
+		ArrayList<Double> lettersProbability = new ArrayList<Double>();
+		for(String key : probabilities.keySet()) {
+			if(key.substring(0, portionSize - 1).equals(target)) {
+				lettersList.add(key);
+				lettersProbability.add(probabilities.get(key));
+			}
+		}
+		return getProbability(lettersList, lettersProbability);
+	}
 
-	String getProbability(String[] lettersList, double[] lettersProbability) {
+	String getProbability(ArrayList<String> lettersList,ArrayList<Double> lettersProbability) {
 		// 1/rank = proabability
 		// Imagine chosing a random angle on a pie chart with different
 		// segmentsItem[] items = ...;
 
 		// Compute the total weight of all items together
 		double totalWeight = 0.0d;
-		for (int i = 0; i < lettersProbability.length; i++) {
-			totalWeight += lettersProbability[i];
+		for (int i = 0; i < lettersProbability.size(); i++) {
+			totalWeight += lettersProbability.get(i);
 		}
 		// Now choose a random item
 		int randomIndex = -1;
 		double random = Math.random() * totalWeight;
-		for (int i = 0; i < lettersList.length; ++i) {
-			random -= lettersProbability[i];
+		for (int i = 0; i < lettersList.size(); ++i) {
+			random -= lettersProbability.get(i);
 			if (random <= 0.0d) {
 				randomIndex = i;
 				break;
 			}
 		}
-		return lettersList[randomIndex];
+		if(randomIndex == -1)
+			return "";
+		return lettersList.get(randomIndex);
 	}
 }
